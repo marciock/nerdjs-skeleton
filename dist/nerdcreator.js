84190e6ff761a90cb61672c3b9de4c759ec955ae
marciock <marciockalves@gmail.com>
2018-07-08 23:03:37 -0300

    nerdcreator

--- dist/nerdcreator.js
+++ dist/nerdcreator.js
@@ -7,8 +7,6 @@ export class Creator extends HTMLElement{
     
     constructor(){
      super();
-      
-      
 
   }
     render(){}
@@ -40,13 +38,27 @@ export class Creator extends HTMLElement{
         let child=el.querySelector('#'+id);
 
         return child;
-
     }
-    
-
-
-
-
-          
+   
 }   //end class
 
+export class Generator extends HTMLElement{
+
+
+   
+    create(){}
+
+    getProps(prop){
+        let myProp=this.getAttribute(prop);
+        return myProp;
+	}
+    setState(state,value){
+        window.state[state]=value;
+        return window.state;
+    }
+    connectedCallback(){
+        this.state=window.state;
+        this.create();
+    }
+
+}
